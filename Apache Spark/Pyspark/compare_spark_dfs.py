76fc1855e30b3bc45b665fe9ac3b254a7ef33dc7
Joelocole <109953061+Joelocole@users.noreply.github.com>
2024-01-04 14:04:01 +0100

    Added new features, and Bugfixing

--- Apache Spark/Pyspark/compare_spark_dfs.py
+++ Apache Spark/Pyspark/compare_spark_dfs.py
@@ -2,13 +2,12 @@ import os
 import sys
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
-from pyspark.sql.types import DoubleType, DateType
+from pyspark.sql.types import DoubleType, DateType, StringType
 import re
 from datetime import datetime
 
 
 def init_spark():
-
     # SPARK INITIALISATION
     # Defining Environment Variables
     os.environ['SPARK_DRIVER_MEMORY'] = "8G"
@@ -25,16 +24,16 @@ def init_spark():
     print(f"Spark version: {spark_session.version}")
     conf = spark_session.sparkContext._conf.setAll(
         [
-            ('spark.app.name', 'BPER Prepayment | Local - Tests'),
+            ('spark.app.name', 'APP Prepayment | Local - Tests'),
             ("spark.sql.execution.arrow.pyspark.enabled", "true"),
             ('spark.driver.maxResultSize', "8G"),
             ("spark.memory.offHeap.enabled", "true"),
             ('spark.memory.offHeap.size', '8G'),
-            ("spark.sql.shuffle.partitions", "6"),
+            ("spark.sql.shuffle.partitions", "10"),
             ("spark.sql.adaptive.enabled", "true"),
             ("spark.sql.autoBroadcastJoinThreshold", "-1"),
-            ("spark.sql.debug.maxToStringFields", "500"),
-            ("spark.executor.memory", "12g")
+            ("spark.sql.debug.maxToStringFields", "300"),
+            ("spark.executor.memory", "13g")
         ]
     )
 
@@ -46,28 +45,30 @@ def init_spark():
 
 
 def assert_dataframes_approx_equal(
-        df1_path, df2_path, sep=";", header=None, inferSchema=None, spark=init_spark(), suffixes=("_OLD", "_NEW"),
-        index_columns=None, start_date=None, end_date=None, ref_date_column=None, date_columns=None,
-        output_csv_path=None, abs_tolerance=1.0e-4):
+        df1_path: str, df2_path: str, sep: str = ";", header: bool = None, infer_schema: bool = None,
+        spark=init_spark(),
+        suffixes: list[str] = ("_OLD", "_NEW"),
+        index_columns: list[str] = None, detailed_regression: bool = False,
+        start_date: str = None, end_date: str = None, ref_date_column: str = None, date_columns: list[str] = None,
+        output_csv_path: str = None, abs_tolerance: float = 1.0e-4):
 
     current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
-    def remove_suffixes(input_string, suffixes):
+    def remove_suffixes(input_string, _suffixes):
         # Create a regex pattern to match any of the specified suffixes at the end of the string
-        pattern = re.compile(f'({"|".join(suffixes)})$')
+        pattern = re.compile(f'({"|".join(_suffixes)})$')
 
         # Use the sub method to replace the matched pattern with an empty string
         result = re.sub(pattern, '', input_string)
 
         return result
 
-    def approx_spark(value, abs_tolerance):
-         return f.when(f.isnull(value) | (f.abs(value) >= abs_tolerance), value).otherwise(None)
+    df1 = spark.read.csv(df1_path, sep=sep, header=header, inferSchema=infer_schema, nanValue="")
+    df2 = spark.read.parquet(df2_path, inferSchema=infer_schema, nanValue="")
 
-    index_columns = [c.upper() for c in index_columns]
-
-    df1 = spark.read.csv(df1_path, sep=sep, header=header, inferSchema=inferSchema,  nanValue="")
-    df2 = spark.read.csv(df2_path, sep=sep, header=header, inferSchema=inferSchema,  nanValue="")
+    # STANDARDISE COLUMNS and DATA
+    ref_date_column_stand = ref_date_column.upper() if ref_date_column else index_columns[0]
+    index_columns_stand = [c.upper() for c in index_columns]
 
     # Convert column names to uppercase
     for col_name in df1.columns:
@@ -75,6 +76,8 @@ def assert_dataframes_approx_equal(
 
     for col_name in df2.columns:
         df2 = df2.withColumnRenamed(col_name, col_name.upper())
+
+    df1 = df1.withColumnRenamed("STRBANK_TR", "STRBANKCODE_TR")
 
     if date_columns:
         date_columns = [date_col.upper() for date_col in date_columns]
@@ -86,58 +89,87 @@ def assert_dataframes_approx_equal(
                 date_col, f.to_date(f.substring(f.regexp_replace(f.col(date_col), "-", ""), 0, 8), 'yyyyMMdd')
             )
 
-    ref_date_column = ref_date_column.upper()
     if start_date and end_date and ref_date_column:
-
-        start_date = f.lit(start_date).cast(DateType())
-        end_date = f.lit(end_date).cast(DateType())
+        _start_date = f.lit(start_date).cast(DateType())
+        _end_date = f.lit(end_date).cast(DateType())
 
         # Filter df_new
-        df1 = df1.filter(f.col(ref_date_column).isNotNull()) \
-            .filter((f.col(ref_date_column) >= start_date) & (f.col(ref_date_column) <= end_date))
+        df1 = df1.filter(f.col(ref_date_column_stand).isNotNull()) \
+            .filter((f.col(ref_date_column_stand) >= _start_date) & (f.col(ref_date_column_stand) <= _end_date))
 
         # Filter df_old
-        df2 = df2.filter(f.col(ref_date_column).isNotNull()) \
-            .filter((f.col(ref_date_column) >= start_date) & (f.col(ref_date_column) <= end_date))
+        df2 = df2.filter(f.col(ref_date_column_stand).isNotNull()) \
+            .filter((f.col(ref_date_column_stand) >= _start_date) & (f.col(ref_date_column_stand) <= _end_date))
 
     # COMPARE DFS
     if ~(df1.count() == df2.count()) & ~(len(df1.columns) == len(df2.columns) & ~(df1.columns == df2.columns)):
 
         # Select numeric columns to check
-        digit_columns = [col.upper() for col in df1.columns if col not in index_columns]
+        digit_columns = [col.upper() for col in df1.columns if col not in index_columns_stand]
 
         for c_name in digit_columns:
+                df1 = df1.withColumn(
+                    c_name + suffixes[0],
+                    f.col(c_name).cast(DoubleType())
+                    .alias(c_name)).drop(c_name)
+                df2 = df2.withColumn(
+                    c_name + suffixes[1],
+                    f.col(c_name).cast(DoubleType())
+                    .alias(c_name)).drop(c_name)
+
+        # Dates, categorical and flag data to string data type
+        null_counts_dict = {}
+        for c_name in index_columns_stand:
+            # if c_name not in date_columns:
             df1 = df1.withColumn(
-                c_name + suffixes[0],
-                f.col(c_name).cast(DoubleType())
-                .alias(c_name)).drop(c_name)
+                c_name,
+                f.col(c_name).cast(StringType())
+            )
             df2 = df2.withColumn(
-                c_name + suffixes[1],
-                f.col(c_name).cast(DoubleType())
-                .alias(c_name)).drop(c_name)
+                c_name,
+                f.col(c_name).cast(StringType())
+            )
+
+            # print(f"showing sql_table {c_name}: {sql_table[[c_name]].distinct().show()}")
+            # print(f"showing spark_table {c_name}: {spark_table[[c_name]].distinct().show()}")
+            sql_table_null_count = \
+            df1.select(f.sum(f.col(c_name).isNull().cast("int")).alias("NullCount")).collect()[0]["NullCount"]
+            spark_table_null_count = \
+            df2.select(f.sum(f.col(c_name).isNull().cast("int")).alias("NullCount")).collect()[0]["NullCount"]
+            null_counts_dict[f"{c_name}{suffixes[0]}"] = sql_table_null_count
+            null_counts_dict[f"{c_name}{suffixes[1]}"] = spark_table_null_count
+
+        for column, count in null_counts_dict.items():
+            print(f"Number of nulls in the '{column}' column: {count}")
+
+        print(f"df1 count: \n{df1.count()}")
+        print(f"df2 count: \n{df2.count()}")
 
         # Order DataFrames by index columns
-        df1 = df1.orderBy(*[f.col(col).desc() for col in index_columns])
-        df2 = df2.orderBy(*[f.col(col).desc() for col in index_columns])
+        df1 = df1.orderBy(
+            [f.col(c).desc() for c in index_columns_stand],
+        )
+        df2 = df2.orderBy(
+            [f.col(c).desc() for c in index_columns_stand],
+        )
 
-        # List of numerical columns for comparison
-        suffixes_0_list = [f"{col}{suffixes[0]}" for col in digit_columns]
-        suffixes_1_list = [f"{col}{suffixes[1]}" for col in digit_columns]
+        # standardizing Null values in index columns
+        for c in index_columns_stand:
+            if df1.schema[c].dataType == DateType():
+                print(f"variable c: {c}")
+                date_fill_value = "2199-01-01"
+                print(f"date_fill_value c: {date_fill_value}")
+                df1 = df1.fillna({c: date_fill_value})
+                df2 = df2.fillna({c: date_fill_value})
+            else:
+                str_fill_value = "@@_test_@@_fill_@@"
+                df1 = df1.fillna({c: str_fill_value})
+                df2 = df2.fillna({c: str_fill_value})
 
-        for col_name in digit_columns:
-            df1 = df1.withColumn(
-                f"{col_name}{suffixes[0]}",
-                approx_spark(f.col(f"{col_name}{suffixes[0]}"), f.lit(abs_tolerance))
-            ).alias(f"{col_name}{suffixes[0]}")
-            df2 = df2.withColumn(
-                f"{col_name}{suffixes[1]}",
-                approx_spark(f.col(f"{col_name}{suffixes[1]}"), f.lit(abs_tolerance))
-            ).alias(f"{col_name}{suffixes[1]}")
-
-        non_regression_result = df1.join(df2, on=index_columns, how="outer")
+        non_regression_result = df1.join(df2, on=index_columns_stand, how="outer")
 
         for col in non_regression_result.columns:
-            if col not in index_columns:
+            if col not in index_columns_stand:
                 col = col[:-len(suffixes[0])] if col.endswith(suffixes[0]) else col[:-len(suffixes[1])]
 
                 if f"{col}_COMPARE" not in non_regression_result.columns:
@@ -145,18 +177,27 @@ def assert_dataframes_approx_equal(
                         f"{col}_COMPARE",
                         f.when(
                             (f.col(f"{col}{suffixes[0]}").isNull() & f.col(f"{col}{suffixes[1]}").isNull()) |
-                            (f.col(f"{col}{suffixes[0]}") == f.col(f"{col}{suffixes[1]}")),
+                            (f.abs(f.col(f"{col}{suffixes[0]}") - f.col(f"{col}{suffixes[1]}")) < f.lit(abs_tolerance)),
                             True
                         ).otherwise(False)
                     )
 
-        # for col in non_regression_result.columns:
-        #     # suff_x = [suff for suff in suffixes]
-        #     if col not in index_columns:
-        #         if col.endswith('_COMPARE'):
-        #             non_regression_result = non_regression_result.filter(
-        #                 ~f.expr(f"{col}")
-        #             )
+        regression_count_dict = {}
+
+        if detailed_regression:
+            # detailed regression getting data
+            for column in non_regression_result.columns:
+                if column.endswith("_COMPARE"):
+                    regressions_count = non_regression_result.filter(f.col(column) == False).count()
+                    regression_count_dict[column] = regressions_count
+
+            print(f"df1 count: \n{df1.count()}")
+            print(f"df2 count: \n{df2.count()}")
+
+            for key, value in regression_count_dict.items():
+                print(f"{key}: {value}")
+
+            print(f"non_regression_result count: \n{non_regression_result.count()}")
 
         temp_cols = [col for col in non_regression_result.columns]
         prefixes = list(set(remove_suffixes(col, suffixes) for col in temp_cols))
@@ -174,9 +215,9 @@ def assert_dataframes_approx_equal(
         unique_list = [x for x in ordered_columns if x not in seen and not seen.add(x)]
 
         non_regression_result = non_regression_result.select(
-            *[ref_date_column] + [c for c in unique_list if c != ref_date_column]
+            *index_columns_stand + [c for c in unique_list if c not in index_columns_stand]
         )
-
+        # non_regression_result.show()
         # Filter out rows where a True value is present in columns ending with "_COMPARE"
         filter_condition = f.expr(
             " OR ".join(f"{col} = False" for col in non_regression_result.columns if col.endswith('_COMPARE')))
@@ -184,10 +225,11 @@ def assert_dataframes_approx_equal(
 
         if not non_regression_result.isEmpty():
             print(f"Dataframes are not identical please check the file {output_csv_path} for a detailed inspection.")
-            non_regression_result.show(non_regression_result.count())
+            non_regression_result.show()
 
-            non_regression_result.toPandas().to_csv(
-                f"{output_csv_path[:-4]}_{current_datetime}.csv", index=True, sep=";", encoding='utf-8'
+            non_regression_result.write \
+                .format("csv").option("header", "true").save(
+                f"C:/Users/osahenrunmwencole/Desktop/Prometeia/Projects/BPER_Prepayment_to_sparkSQL/local_tests/{output_csv_path[:-4]}_{current_datetime}.csv"
             )
 
         # Cleaning columns Suffixes before schema assertion
@@ -198,12 +240,15 @@ def assert_dataframes_approx_equal(
 
         assert df1.count() == df2.count(), f"Dfs Counts: Df1: {df1.count()}, Df2: {df2.count()}"
 
-        assert df1.schema == df2.schema, f"Dataframes schemas are different: \nDf1 Schema: {df1.printSchema()}\nDf2 Schema: {df2.printSchema()}"
+        assert df1.schema == df2.schema, (f"Dataframes schemas are different: "
+                                          f"\nDf1 Schema: {df1.printSchema()}"
+                                          f"\nDf2 Schema: {df2.printSchema()}")
 
         if non_regression_result.count() > 0:
             non_regression_result.show()
 
-        print("=== ✔️ Test Passed. Dataframes are identical. ===")
+        print("========= ✔️ Test Passed. Dataframes are identical. =========")
+
     else:
         raise ValueError(
             f"DataFrames have different shapes and cannot be compared."
@@ -213,12 +258,35 @@ def assert_dataframes_approx_equal(
 
 
 if __name__ == "__main__":
-    df1_path = "C:/Users/osahenrunmwencole/Downloads/tassi_varmacro_mp.csv"
-    df2_path = "C:/Users/osahenrunmwencole/Desktop/Prometeia/Projects/BPER_Prepayment_to_sparkSQL/local_tests/tassi_varmacro_local.csv"
 
-    index_columns = ["data_oss"]
+    BASE_PATH = "C:/Users/osahenrunmwencole/"
+
+    # application_decay_grouped PARAMETERS
+    PATH_tassi_varmacro_OLD = BASE_PATH + "Downloads//tassi_varmacro_mp.csv"
+    PATH_tassi_varmacro_NEW = BASE_PATH + "Desktop/Prometeia/Projects/BPER_Prepayment_to_sparkSQL/local_tests/tassi_varmacro_local.csv"
+
+    # index_cols = [
+    #     "data_oss"
+    # ]
+    # ref_date_col = "data_oss"
+    # date_cols = ["data_oss"]
+
+    # application_decay_grouped PARAMETERS
+    PATH_application_decay_grouped_OLD = BASE_PATH + "Downloads/DB_POSTGRE/l1_varmacro_application_202312250033.csv"  # DB POSTGRE
+    PATH_application_decay_grouped_NEW = BASE_PATH + "Downloads/BLOB/L1_varmacro_application.parquet"  # BLOB
+
+    index_cols = [
+        "data_oss",
+    ]
+
+    # ref_date_col = "dtCutOff"
+    date_cols = ["data_oss"]
 
     assert_dataframes_approx_equal(
-        df1_path, df2_path, header=True, inferSchema=False, sep=";", abs_tolerance=0.0001,
-        index_columns=index_columns, date_columns=["data_oss"], suffixes=("_SQL", "_SPARK"), output_csv_path="conflicting_df.csv",
-        ref_date_column="data_oss", start_date="1990-01-31", end_date="2022-09-30")
+        PATH_application_decay_grouped_OLD, PATH_application_decay_grouped_NEW,
+        header=True, infer_schema=False, sep=";",
+        # abs_tolerance=0.0001,
+        index_columns=index_cols, #date_columns=date_cols,
+        suffixes=("_SQL", "_SPARK"),
+        output_csv_path="conflicting_df_application_grouped.csv")
+        # ref_date_column=ref_date_col, start_date="1990-01-31", end_date="2022-09-30")
